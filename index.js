68cc3cd0dfe5736e33ed9b454e18fbed85430e19
Viskhan <rustam1987@mail.ru>
2023-06-04 10:06:00 +0300

    реализовал соединение с базой данных и запустил сервер

---
+++ index.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+//app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGO_SERVER, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+app.listen(process.env.PORT, () => {
+   console.log('Connected...');
+});
