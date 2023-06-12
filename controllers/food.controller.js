47b84908d83b7fbe17ed4c96c404567bc8d7b295
Viskhan <rustam1987@mail.ru>
2023-06-12 09:59:03 +0300

    реализовал foodController, в моделях сделал все значения обязательными

--- controllers/food.controller.js
+++ controllers/food.controller.js
@@ -1,5 +1,59 @@
 const Food = require('../models/Food.model');
 
 module.exports.foodController = {
-   
+   getFood: async (req, res) => {
+      try {
+         const data = await Food.find();
+         res.json(data);
+      } catch (error) {
+         res.json(error + '. Ошибка при получении меню');
+      }
+   },
+   getFoodByCategory: async (req, res) => {
+      try {
+         const data = await Food.find({ category: req.params.id });
+         res.json(data);
+      } catch (error) {
+         res.json(error + '. Ошибка при получении меню по категориям');
+      }
+   },
+   getFoodById: async (req, res) => {
+      try {
+         const data = await Food.findById(req.params.id);
+         res.json(data);
+      } catch (error) {
+         res.json(error + '. Ошибка при получении блюды');
+      }
+   },
+   postFood: async (req, res) => {
+      try {
+         const { name, urlImage, price, size, category } = req.body;
+         await Food.create({ name, urlImage, price, size, category });
+         res.json('Успешно добавлено');
+      } catch (error) {
+         res.json(error + '. Ошибка при добавлении блюды');
+      }
+   },
+   updateFood: async (req, res) => {
+      try {
+         const { name, urlImage, price, size, category } = req.body;
+         await Food.findByIdAndUpdate(req.params.id, {
+            name,
+            urlImage,
+            price,
+            size,
+            category,
+         });
+      } catch (error) {
+         res.json(error + '. Ошибка при обновлении блюды');
+      }
+   },
+   deleteFood: async (req, res) => {
+      try {
+         await Food.findByIdAndRemove(req.params.id);
+         res.json('Удалено');
+      } catch (error) {
+         res.json(error + '. Ошибка при удалении блюды');
+      }
+   },
 };
--- models/Food.model.js
+++ models/Food.model.js
@@ -1,23 +1,26 @@
 const { Schema, model, SchemaTypes } = require('mongoose');
 
 const FoodSchema = Schema({
-    name: {
-        type: String,
-        required: true,
-     },
-     urlImage: {
-        type: String,
-        required: true,
-     },
-     price: {
-        type: Number,
-        required: true,
-     },
-     size: Number,
-     category: {
+   name: {
+      type: String,
+      required: true,
+   },
+   urlImage: {
+      type: String,
+      required: true,
+   },
+   price: {
+      type: Number,
+      required: true,
+   },
+   size: {
+      type: Number,
+      default: 1,
+   },
+   category: {
       ref: 'Category',
-      type: SchemaTypes.ObjectId
-     }
+      type: SchemaTypes.ObjectId,
+   },
 });
 
 const Food = model('Food', FoodSchema);
