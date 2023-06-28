1dc5a075dcd4328465df873bd7cadff0d4dc5580
Viskhan <rustam1987@mail.ru>
2023-06-28 22:06:53 +0300

    добавил статусы в products.controller

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -4,34 +4,34 @@ module.exports.productController = {
    getProducts: async (req, res) => {
       try {
          const data = await Product.find();
-         res.json(data);
+         res.status(200).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка при получении меню');
+         res.status(404).json(error + '. Ошибка при получении меню');
       }
    },
    getProductsByCategory: async (req, res) => {
       try {
          const data = await Product.find({ category: req.params.id });
-         res.json(data);
+         res.status(200).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка при получении меню по категориям');
+         res.status(404).json(error + '. Ошибка при получении меню по категориям');
       }
    },
    getProductById: async (req, res) => {
       try {
          const data = await Product.findById(req.params.id);
-         res.json(data);
+         res.status(200).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка при получении блюды');
+         res.status(404).json(error + '. Ошибка при получении блюды');
       }
    },
    postProduct: async (req, res) => {
       try {
          const { name, urlImage, price, size, category } = req.body;
          await Product.create({ name, urlImage, price, size, category });
-         res.json('Успешно добавлено');
+         res.status(201).json('Успешно добавлено');
       } catch (error) {
-         res.json(error + '. Ошибка при добавлении блюды');
+         res.status(400).json(error + '. Ошибка при добавлении блюды');
       }
    },
    updateProduct: async (req, res) => {
@@ -44,16 +44,17 @@ module.exports.productController = {
             size,
             category,
          });
+         res.status(200).json('Успешно изменен')
       } catch (error) {
-         res.json(error + '. Ошибка при обновлении блюды');
+         res.status(404).json(error + '. Ошибка при обновлении блюды');
       }
    },
    deleteProduct: async (req, res) => {
       try {
          await Product.findByIdAndRemove(req.params.id);
-         res.json('Удалено');
+         res.status(200).json('Удалено');
       } catch (error) {
-         res.json(error + '. Ошибка при удалении блюды');
+         res.status(404).json(error + '. Ошибка при удалении блюды');
       }
    },
 };
