a7b08399953ef292b112c670214a154b2394a12a
Viskhan <rustam1987@mail.ru>
2023-06-28 16:08:02 +0300

    изменил модель и контроллер cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,47 +1,87 @@
-const CartProduct = require('../models/CartProduct.model');
+const Cart = require('../models/Cart.model');
 
 module.exports.cartController = {
    getCart: async (req, res) => {
       try {
-         const data = await Cart.find().populate('product');
-         data === null ? res.json('Корзина пуста') : res.json(data);
+         const data = await Cart.findOne({ user: req.params.id }).populate('products');
+         data === null ? res.status(200).json('Корзина пуста') : res.status(200).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка загрузки корзины');
+         res.status(404).json(error + '. Ошибка загрузки корзины');
       }
    },
    postToCart: async (req, res) => {
       try {
          const { user, product, quantity, shipping, delivery_status, payment_status } = req.body;
-         const data = await Cart.findById({ user: user });
-         if (data.products.length > 0) {
-         } else if (data === null) {
-            await Cart.create({ user });
+         let data = await Cart.findOne({ user: user });
+         if (!data) {
+            data = new Cart({
+               user: user,
+               products: [{ product, quantity }],
+            });
          } else {
-            await Cart.create({ food, total, count });
+            const index = data.products.findIndex((prod) => prod.product === product);
+            if (index !== -1) {
+               data.products[index].quantity += quantity;
+            } else {
+               data.products.push({ product, quantity });
+            }
          }
-         res.json('Добавлен в корзину');
+         await data.save();
+         res.status(201).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка при добавлении в корзину');
+         res.status(400).json(error + '. Ошибка при добавлении в корзину');
       }
    },
    updateCart: async (req, res) => {
       try {
-         const { total, count } = req.body;
-         const data = await Cart.findById(req.params.id);
-         data.total += total;
-         data.count += count;
-         await data.save();
-         res.json('Изменен');
+         const { user, product, quantity } = req.body;
+         const data = await Cart.findOne({ user: user });
+         const index = data.prod.findIndex((prod) => prod.product === product);
+         if (index !== -1) {
+            data.products[index].quantity = quantity;
+            await data.save();
+            res.status(200).json(data);
+         } else {
+            res.status(404).json('Продукт не найден в корзине');
+         }
       } catch (error) {
-         res.json(error + '. Ошибка при изменении кол-ва продуктов');
+         res.status(404).json(error + '. Ошибка при изменении кол-ва продуктов');
+      }
+   },
+   updateStatusCart: async (req, res) => {
+      try {
+         const [user, shipping, delivery_status, payment_status] = req.body;
+         const data = Cart.findOne({ user: user });
+         data.shipping = shipping;
+         data.delivery_status = delivery_status;
+         data.payment_status = payment_status;
+         await data.save();
+         res.status(200).json(data)
+      } catch (error) {
+         res.status(400).json(error + '. Ошибка при обновлении статусов корзины')
       }
    },
    deleteFromCart: async (req, res) => {
       try {
-         await Cart.findByIdAndRemove(req.params.id);
-         res.json('Блюда ');
+         const { user, product } = req.body;
+         const data = await Cart.findOne({ user: user });
+         data.products = data.products.filter((prod) => prod.product !== product);
+         await data.save();
+         res.status(200).json(data);
       } catch (error) {
-         res.json(error + '. Ошибка при удалении из корзины');
+         res.status(404).json(error + '. Ошибка при удалении из корзины');
+      }
+   },
+   clearCart: async (req, res) => {
+      try {
+         const [user] = req.body;
+         const data = await Cart.findOne({ user: user });
+         data.products = [];
+         data.shipping = null;
+         data.delivery_status = null;
+         data.payment_status = null;
+      } catch (error) {
+         res.status(404).json(error + '. Ошибка при очищении корзины');
       }
    },
 };
--- models/Cart.model.js
+++ models/Cart.model.js
@@ -2,10 +2,43 @@ const { Schema, model, SchemaTypes } = require('mongoose');
 
 const CartSchema = Schema(
    {
+      products: [
+         {
+            product: {
+               ref: 'Product',
+               type: SchemaTypes.ObjectId,
+               unique: true,
+               required: true,
+            },
+            quantity: {
+               type: Number,
+               default: 1,
+               required: true,
+            },
+            price: {
+               type: Number,
+               required: true,
+            }
+         },
+      ],
       user: {
          ref: 'User',
          type: SchemaTypes.ObjectId,
          required: true,
+      },
+      shipping: {
+         type: String,
+         default: 'самовывоз',
+         required: true,
+      },
+      delivery_status: {
+         type: String,
+         default: 'в ожидании',
+         required: true,
+      },
+      payment_status: {
+         type: String,
+         default: "не оплачен",
       },
    },
    { timestamps: true },
